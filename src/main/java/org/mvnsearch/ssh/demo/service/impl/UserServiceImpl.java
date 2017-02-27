9de482548a5959854dcb18c49ad015cbdd208864
linux_china <linux_china@hotmail.com>
2017-02-27 17:38:26 +0800

    user service implementation

---
+++ src/main/java/org/mvnsearch/ssh/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.ssh.demo.service.impl;
+
+import org.mvnsearch.ssh.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * user service implementation
+ *
+ * @author linux_china
+ */
+@Service
+public class UserServiceImpl implements UserService {
+}
