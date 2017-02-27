f1e47e6011d2c17f9272df0f1b31179b2dbed070
linux_china <linux_china@hotmail.com>
2017-02-27 17:37:53 +0800

    struts 2 configuration

---
+++ src/main/java/org/mvnsearch/ssh/demo/config/Struts2Configuration.java
@@ -0,0 +1,27 @@
+package org.mvnsearch.ssh.demo.config;
+
+import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.DispatcherType;
+
+/**
+ * struts 2 configuration
+ *
+ * @author linux_china
+ */
+@Configuration
+public class Struts2Configuration {
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new StrutsPrepareAndExecuteFilter());
+        registration.addUrlPatterns("*.action");
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.FORWARD);
+        registration.setName("StrutsPrepareAndExecuteFilter");
+        return registration;
+    }
+
+}
